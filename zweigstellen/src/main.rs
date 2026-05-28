0acc9679e3a90b8a9faf58d42f3fd400b25db22e
إله <fucker.shhsh@gmail.com>
2026-05-28 11:19:57 +0100

    uhhhhh

---
+++ zweigstellen/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    let num = 1;
+    if num > 3 {
+    	println!("Die Bedingung ist wahr");
+    } else {
+    	println!("Die Bedingung ist falsch");
+    }
+}
---
+++ zweigstellen/target/debug/zweigstellen.d
@@ -0,0 +1,1 @@
+/data/data/com.termux/files/home/rustlernen/zweigstellen/target/debug/zweigstellen: /data/data/com.termux/files/home/rustlernen/zweigstellen/src/main.rs
