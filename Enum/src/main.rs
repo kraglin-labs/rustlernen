f7429bcbbc3b25ecaf26be56f2cdde76af765d4b
Aleksy <notgonnasay@asshole.com>
2026-07-01 10:58:05 +0100

    Bogus Binted

---
+++ Enum/src/main.rs
@@ -0,0 +1,67 @@
+// fn main() {
+//     enum IpAddrKind {
+//         V4,
+//         V6,
+//     }
+
+//     struct IpAddr {
+//         kind: IpAddrKind,
+//         address: String,
+//     }
+
+//     let _four: IpAddrKind = IpAddrKind::V4;
+//     let _six: IpAddrKind = IpAddrKind::V6;
+
+
+//     fn route(_ip_kind: IpAddrKind) {
+        
+//     }
+
+//     route(IpAddrKind::V4);
+//     route(IpAddrKind::V6);
+
+// }   
+
+
+// enum IpAddrKind {
+//     V4,
+//     V6,
+// }
+
+// struct IpAddr {
+//     kind: IpAddrKind,
+//     address: String,
+// }
+
+// fn main(){
+//     let home = IpAddr {
+//         kind: IpAddrKind::V4,
+//         address: "127.0.0.1".to_string(),
+//     };
+//     let loop_back = IpAddr {
+//         kind: IpAddrKind::V6,
+//         address: "::1".to_string(),
+//     };
+
+
+
+// }
+
+// enum IpAddrKind {
+//     V4(String),
+//     V6(String),
+// }
+
+// fn main() {
+//     let home = IpAddrKind::V4(String::from("127.0.0.1"));
+//     let loopback = IpAddrKind::V6(String::from("::1"))
+// }
+enum IpAddrKind {
+    V4(u8, u8, u8, u8),
+    V6(String),
+}
+
+fn main() {
+    let home = IpAddrKind::V4(127, 0, 0, 1);
+    let loopback = IpAddrKind::V6(String::from("::1"));
+}
--- enumerationn/src/main.rs
+++ enumerationn/src/main.rs
@@ -1,37 +1,165 @@
 
+// struct IpV4Addr() {
+
+// }
+// struct IpV6Addr() {
+
+// }
+// enum IpAddr {
+//     V4(IpV4Addr),
+//     v6(IpV6Addr),
+// }
+// fn main(){
+
+// }
+//This 
+// enum Game {
+//     Move(x: i32, y: i32),
+//     Quit,
+//     Write(String),
+//     ChangeColour(i32, i32, i32),
+// }
+// //Is the same thing as this:
+// struct Move{
+//     x: i32,
+//     y: i32,
+// }
+// struct Quit; //This ist a unit struct
+// struct ChangeColour(i32, i32, i32); // Tuple struct
+// struct Write(String); //Tuple struct also
+//There can also be methods:
+// #[derive(Debug)]
+// enum Game {
+//     Move{x: i32, y: i32},
+//     Quit,
+//     Write(String),
+//     ChangeColour(i32, i32, i32),
+// }
+
+// impl Game {
+//     fn call(&self) {
+//         let mut neue = Vec::new();
+//         neue.push(&self);
+
+//         for neu in neue {
+//             println!("{:?}", neu);
+//         }
+//     }
+// }
+
+// fn main() {
+//     let m = Game::Move{x: 3, y: 4};
+//     m.call();
+// }
+
+//the option enum
+
+// enum Option<T> {
+//     None,
+//     Some(T),
+// }
+
+// fn main() {
+//     let number = Some(5);
+//     let name = Some("Sakis");
+//     let nonexistent_number: Option<i32> = None;
+
+//     match number {
+//         Some(value) => println!("Value: {}", value),
+//         None => println!("Value: None"),
+//     }
+//     match name {
+//         Some(value) => println!("Value: {}", value),
+//         None => println!("Value: None"),
+//     }
+//     match nonexistent_number {
+//         Some(value) => println!("Value: {}", value),
+//         None => println!("Value: None"),
+//     }
+// }
+
+// #[allow(dead_code)]
+// enum Coins {
+//     Penny(u32),
+//     Nickel(u32),
+//     Dime(u32),
+//     Quarter(u32),
+//     Dollar(u32),
+// }
+
+// fn value_in_cents(coin: Coins) -> u32 {
+//     match coin {
+//         Coins::Penny(val) => 1 * val,
+//         Coins::Nickel(val) => 5 * val,
+//         Coins::Dime(val) => 10 * val,
+//         Coins::Quarter(val) => 25 * val,
+//         Coins::Dollar(val) => 100 * val,
+//     }
+// }
+
+// fn main() {
+//     let nummer: u32 = 3219;
+//     let value = "n";
+//     let buck: Coins = {
+//         match value {
+//             "p" => Coins::Penny(nummer),
+//             "n" => Coins::Nickel(nummer),
+//             "d" => Coins::Dime(nummer),
+//             "q" => Coins::Quarter(nummer),
+//             "dl" => Coins::Dollar(nummer),
+//             _ => Coins::Dime(nummer),
+//         }
+//     };
+//     println!("{} cents", value_in_cents(buck));
+// }
+
+// fn main(){
+//     let config_max = Some(3u8);
+//     match config_max {
+//         Some(max) => {println!("Max value: {max}")},
+//         _ => (),
+//     }
+
+// }
+
+// fn main(){
+//     let config_max = Some(3u8);
+
+//     if let Some(maximum) = config_max {
+//         println("The max is {}", maximum);
+//     }
+// }
 #[derive(Debug)]
-enum IpAddrKind {
-    Ipv4,
-    Ipv6,
+#[allow(dead_code)]
+enum SchoolMember {
+    Student {name: String, cgpa: f32},
+    Teacher {name: String, id: String},
+    RegularStaff {name: String, id: String},
 }
 
+#[allow(dead_code)]
+fn get_info_lame(person: SchoolMember) -> (Option<String>, Option<f32>) {
+    if let SchoolMember::Student{name, cgpa} = person {
+        return (Some(name), Some(cgpa))
+    } else {
+        return (None, None);
+    };
+}
 
+fn get_info_cool(person: SchoolMember) -> (Option<String>, Option<f32>) {
+    let SchoolMember::Student{name, cgpa} = person else {
+        return (None, None);
+    };
+    (Some(name), Some(cgpa))
+}
 fn main() {
-    let four = IpAddrKind::Ipv4;
-    let six = IpAddrKind::Ipv6;
-
-    route(four);
-    route(six);
-
-    let home = IpAddr {
-        kind: IpAddrKind::Ipv4,
-        address: "102.89.82.148".to_string(),
-    };
-    let loopback = IpAddr {
-        kind: IpAddrKind::Ipv6,
-        address: "::1".to_string(),
-    };
-
-    println!("{:?}", home);
-    println!("{:?}", loopback);
-}
-
-fn route(ip_type: IpAddrKind) {
-    println!("{:?}", ip_type)
-}
-
-#[derive(Debug)]
-struct IpAddr {
-    kind: IpAddrKind,
-    address: String,
+    let person1 = SchoolMember::Student{name: "Damien".to_string(), cgpa: 4.7f32};
+    let person2 = SchoolMember::Teacher{name: "Alek".to_string(), id: "39151f".to_string()};
+    
+    if let (Some(name), Some(grade)) = get_info_cool(person2) {
+        println!("{} has GPA: {}", name, grade);
+    } else {
+        println!("Not a student");
+    }
+    
 }
