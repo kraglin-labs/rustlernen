d75bd6a0febab9314ac316638485fba180406724
Aleksy <notgonnasay@asshole.com>
2026-07-04 19:10:55 +0100

    made Enum

--- Enum/src/main.rs
+++ Enum/src/main.rs
@@ -1,68 +1,174 @@
-// fn main() {
-//     enum IpAddrKind {
-//         V4,
-//         V6,
-//     }
+use std::io::{self, Read};
 
-//     struct IpAddr {
-//         kind: IpAddrKind,
-//         address: String,
-//     }
-
-//     let _four: IpAddrKind = IpAddrKind::V4;
-//     let _six: IpAddrKind = IpAddrKind::V6;
-
-
-//     fn route(_ip_kind: IpAddrKind) {
-        
-//     }
-
-//     route(IpAddrKind::V4);
-//     route(IpAddrKind::V6);
-
-// }   
-
-
-// enum IpAddrKind {
-//     V4,
-//     V6,
-// }
-
-// struct IpAddr {
-//     kind: IpAddrKind,
-//     address: String,
-// }
-
-// fn main(){
-//     let home = IpAddr {
-//         kind: IpAddrKind::V4,
-//         address: "127.0.0.1".to_string(),
-//     };
-//     let loop_back = IpAddr {
-//         kind: IpAddrKind::V6,
-//         address: "::1".to_string(),
-//     };
-
-
-
-// }
-
-// enum IpAddrKind {
-//     V4(String),
-//     V6(String),
-// }
-
-// fn main() {
-//     let home = IpAddrKind::V4(String::from("127.0.0.1"));
-//     let loopback = IpAddrKind::V6(String::from("::1"))
-// }
-#[allow(unused)]
-enum IpAddrKind {
-    V4(u8, u8, u8, u8),
-    V6(String),
+#[derive(Debug)]
+struct Wallet {
+    owner: String,
+    balance: f32,
+    transactions: Vec<Transaction>,
+    new_transaction: Transaction,
+    password: String,
 }
 
+#[derive(Debug)]
+enum Transaction {
+    Transfer{name: String, amount: f32},
+    Withdraw(f32),
+    Deposit(f32),
+    None,
+}
+
+impl Wallet {
+    fn new(owner: &str, password: &str) -> Self {
+        Self {
+            owner: owner.to_string(),
+            balance: 0.0,
+            transactions: Vec::new(),
+            new_transaction: Transaction::None,
+            password: password.to_string(),
+        }
+    }
+    fn transaction(&mut self, trans_type: Transaction) {
+        match trans_type {
+            Transaction::Withdraw(amount) => {
+                let neue = amount + (amount * 0.02);
+                loop {
+                    let mut pass = String::new();
+                    println!("Enter the password:");
+                    io::stdin().read_line(&mut pass).expect("Unable to read line");
+
+                    if pass.trim() == self.password {
+                        println!("Withdrawing {} with {} charges", amount, neue);
+                        withdraw(self, amount);
+                        self.transactions.push(Transaction::Withdraw(neue));
+                        break;
+                    } else {
+                        if pass.trim() == "exit" {
+                            println!("exiting...");
+                            break;
+                        } else {
+                            println!("Wrong password");
+                        }
+                    }
+                }
+
+            },
+            Transaction::Deposit(amount) => {
+                let neue = amount - (amount * 0.02);
+                loop {
+                    println!("Enter the password");
+                    let pass = input();
+                    if pass.trim() == self.password {
+                        println!("Removing 2% charges from {}, depositing {}", amount, neue);
+                        deposit(self, neue);
+                        self.transactions.push(Transaction::Withdraw(amount));
+                        break;
+                    } else {
+                        if pass.trim() == "exit" {
+                            println!("exiting...");
+                            break;
+                        } else {
+                            println!("Wrong password");
+                        }
+                    }
+
+                }
+                
+            },
+            Transaction::Transfer{name, amount} => {
+                let neue = amount + (amount * 0.02);
+                loop {
+                    println!("Enter the password");
+                    let pass = input();
+                    if pass.trim() == self.password {
+                        if neue < self.balance{
+                            println!("Removing 2% charges from {}, Transferring {}", amount, neue);
+                            transfer(self, name, neue);
+                            self.transactions.push(Transaction::Withdraw(neue));
+                            break;
+                        } else {
+                            println!("Balance not up to {}", neue);
+                            continue;
+                        }
+                        
+                    } else {
+                        if pass.trim() == "exit" {
+                            println!("exiting...");
+                            break;
+                        } else {
+                            println!("Wrong password");
+                        }
+                    }
+
+                }
+                
+            },
+            _ => (),
+        }
+    }
+}
+
+fn withdraw(person: &mut Wallet, amount: f32) {
+    person.balance += amount;
+    println!("Successfully withdrawn {} to {}", amount, person.owner);
+}
+fn deposit(person: &mut Wallet, amount: f32) {
+    person.balance += amount;
+    println!("Successfully deposited {} to {}", amount, person.owner);
+}
+fn transfer(person: &mut Wallet, user: String, amount: f32) {
+    person.balance -= amount;
+    println!("Successfull transferred {} from {} to {} ", amount, person.owner, user)
+}
+fn input() -> String {
+    let mut password = String::new();
+    io::stdin().read_line(&mut password).expect("Unable to read line");
+    password
+}
 fn main() {
-    let _home = IpAddrKind::V4(127, 0, 0, 1);
-    let _loopback = IpAddrKind::V6(String::from("::1"));
+    let mut user = Wallet::new("Sakis", "sakis");
+    loop {
+        turn(&mut user);
+    }
+    println!("{:#?}", user)
+}
+
+fn turn(usr: &mut Wallet) {
+    println!("What do you wanna do?");
+    let words = input();
+    let word: Vec<&str> = words.split_whitespace().collect();
+    let neuewort: Vec<String> = word.iter().map(|word| word.to_lowercase()).collect();
+    match neuewort[0].as_str() {
+        "withdraw" => {
+            let amount = neuewort[1].parse().unwrap();
+            usr.transaction(Transaction::Withdraw(amount));
+        },
+        "transfer" => {
+            let amount = neuewort[1].parse().unwrap();
+            let name = neuewort[3].clone();
+            usr.transaction(Transaction::Transfer { name, amount });
+        },
+        "deposit" => {
+            let amount = neuewort[1].parse().unwrap();
+            usr.transaction(Transaction::Deposit(amount));
+        },
+        "show" => {
+            match neuewort[1].as_str() {
+                "balance" => {
+                    println!("{}", usr.balance);
+                },
+                "history" => {
+                    println!("{:?}", usr.transactions);
+                },
+                "password" => {
+                    println!("{}", usr.password);
+                }
+                _ => {
+                    println!("not applicable");
+                    println!("You can either:\nshow balance\nshow history\nshow password");
+                }
+            }
+        }
+        _ => ()
+        
+    }
 }
---
+++ packages/src/main.rs
@@ -0,0 +1,2 @@
+//src/main.rs for a binary, src/lib.rs for a binary
+//
